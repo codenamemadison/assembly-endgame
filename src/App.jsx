b752e70f9e76a7c33a8e628a9880d44870f2ead4
Madison Shimbo <42008799+codenamemadison@users.noreply.github.com>
2025-07-22 21:26:25 -0400

    create language boxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { languages } from "./languages.js"
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const languageElements = languages.map((lan) => {
+      return (
+          <div 
+              style={{
+                  backgroundColor: lan.backgroundColor,
+                  color: lan.color
+              }}
+              key={lan.name}
+          >{lan.name}</div>
+      )
+  })
   return (
     <main>
         <header>
             <h1>Assembly: Endgame</h1>
             <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
         </header>
+        <section id="status-bar">
+            <h2>You win! </h2>
+            Well done 🎉
+        </section>
+        <div id="languages-container">
+          {languageElements}
+        </div>
     </main>
   )
 }
